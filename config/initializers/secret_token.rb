fcb2d3cf44032542c0c3ec69e9bcf96c91ebec6f
Diogo Santinon <diogosantinon@gmail.com>
2012-06-21 11:21:32 -0300

    Commit do projeto
    views iniciais
    controles
    models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '573744f07dc5588e4d02e43e206a80af727be6a02b10db1593ca15e78ed0c33bc26f5d085b27a15890661343b6ceb1926420b74c3f44cccba53941bf89a6f5fe'
